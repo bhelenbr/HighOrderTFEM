9f6d9c9e784f29444ce4c1e7a24f8ba0b8f62b30
Egan Johnson <egancjohnson@gmail.com>
2024-06-25 14:32:45 -0400

    Merge branch 'master' of https://github.com/EganJ/HighOrderTFEM into mesh

---
+++ run_cpu.slurm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --partition=general
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --gpus-per-node=0
+#SBATCH --job-name=CPU_FEM
+#SBATCH --output=logs/cpu_%j.out
+
+date
+
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+./bin/cpu/demo "$@"
+
+
+echo "Done!"
+date
